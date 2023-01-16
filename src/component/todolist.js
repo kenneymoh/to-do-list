dc1959f6233553360235808098fc2a9912e2a522
kennedy <kenneyrit@gmail.com>
2023-01-16 14:41:45 +0300

    done

--- src/component/todolist.js
+++ src/component/todolist.js
@@ -1,10 +1,12 @@
 import ToDoItem from "./todoitem";
-function ToDoList(){
-    return(
-        <div>
-            <ToDoItem />
-        </div>
-    )
+
+function ToDoList(props) {
+  return (
+    <div>
+      {props.todo.map((item, index) => {
+        <ToDoItem key={index} item={item} />;
+      })}
+    </div>
+  );
 }
 export default ToDoList;
-
--- src/views/Home.js
+++ src/views/Home.js
@@ -1,12 +1,27 @@
+import { useState } from "react";
 import Footer from "../component/footer";
 import NavBar from "../component/navBar";
 import ToDoList from "../component/todolist";
 
 function Home(){
+    const[ todo, setToDo] = useState([
+        {
+             "id": 1,
+             "title": "Go to the market",
+            "description": "buy some groceries"
+        
+         },
+    //     {
+    //         "id": 2,
+    //         "title": "learn react",
+    //         "description": "very nice, very nice"
+    //     }
+    ])
+    // console.log(todo);
     return(
         <div>
             <NavBar />
-            <ToDoList />
+            <ToDoList todo = {todo}/>
             <Footer />
         </div>
     )
